c5762b7433d7a2b0ad23a52515689cc363c0a649
藤川 <fujikawa@hcfukuoka.com>
2026-06-04 08:35:09 +0900

    clean HTML no inline base64

---
+++ pages/api/template.js
@@ -0,0 +1,5 @@
+const { TEMPLATE_B64 } = require('../../lib/template');
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json({ b64: TEMPLATE_B64 });
+}
